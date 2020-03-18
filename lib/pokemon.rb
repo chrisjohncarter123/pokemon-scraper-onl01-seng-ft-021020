51b4391222226d4c9a9d56ebc3d5485750780372
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-18 03:43:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,12 @@
 class Pokemon
+  attr_accessor :name, :type
+  
+  attr_reader :id, :db
+  def initialize( name, type, db, id=nil,)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+    
+  end
 end
